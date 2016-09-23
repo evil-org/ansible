a6ecd827a5ea1147237b250a06aca679f33dbaaa
Mark S. Maglana <mmaglana@gmail.com>
2016-09-23 13:33:26 -0700

    Others

--- test/units/modules/extras/cloud/somebodyscomputer/test_firstmod.py
+++ test/units/modules/extras/cloud/somebodyscomputer/test_firstmod.py
@@ -2,6 +2,8 @@
 from __future__ import (absolute_import, division)
 __metaclass__ = type
 
+from urllib2 import URLError
+
 from ansible.compat.tests import unittest
 from ansible.compat.tests.mock import call, create_autospec, patch
 from ansible.module_utils.basic import AnsibleModule
@@ -16,7 +18,7 @@ class TestFirstMod(unittest.TestCase):
         # Setup
         url = "https://www.google.com"
 
-        # mock the return value of fetch_url
+        # mock the return value of open_url
         stream = open_url.return_value
         stream.read.return_value = "<html><head></head><body>Hello</body></html>"
         stream.getcode.return_value = 200
@@ -32,6 +34,17 @@ class TestFirstMod(unittest.TestCase):
 
         expected = call(url)
         self.assertEqual(expected, open_url.call_args)
+
+    @patch('ansible.modules.extras.cloud.somebodyscomputer.firstmod.open_url')
+    def test__fetch__not_found(self, open_url):
+        # Setup
+        url = "http://blahblah.non.existent"
+
+        open_url.side_effect = URLError("Unknown URL")
+
+        # Exercise
+        with self.assertRaises(URLError):
+            firstmod.fetch(url)
 
     @patch('ansible.modules.extras.cloud.somebodyscomputer.firstmod.write')
     @patch('ansible.modules.extras.cloud.somebodyscomputer.firstmod.fetch')
@@ -67,7 +80,36 @@ class TestFirstMod(unittest.TestCase):
 
     @patch('ansible.modules.extras.cloud.somebodyscomputer.firstmod.write')
     @patch('ansible.modules.extras.cloud.somebodyscomputer.firstmod.fetch')
-    def test__save_data__failure(self, fetch, write):
+    def test__save_data__fetch_failure(self, fetch, write):
+        # Setup
+        mod_cls = create_autospec(AnsibleModule)
+        mod = mod_cls.return_value
+        mod.params = dict(
+            url="https://www.google.com",
+            dest="/tmp/firstmod.txt"
+        )
+
+        fetch.side_effect = URLError("Unknown URL")
+
+        # Exercise
+        firstmod.save_data(mod)
+
+        # Verify
+        self.assertEqual(1, mod.fail_json.call_count)
+
+        expected = call(msg="Data could not be retrieved")
+        self.assertEqual(expected, mod.fail_json.call_args)
+
+        self.assertEqual(1, fetch.call_count)
+
+        expected = call(mod.params["url"])
+        self.assertEqual(expected, fetch.call_args)
+
+        self.assertEqual(0, write.call_count)
+
+    @patch('ansible.modules.extras.cloud.somebodyscomputer.firstmod.write')
+    @patch('ansible.modules.extras.cloud.somebodyscomputer.firstmod.fetch')
+    def test__save_data__write_failure(self, fetch, write):
         # Setup
         mod_cls = create_autospec(AnsibleModule)
         mod = mod_cls.return_value
